c81d7e2b6fb30bbec7f464e5429885aaab70ed01
SerschFischer <sergej.knjasev@me.com>
2022-02-23 13:33:17 +0100

    AutoControllerTests  acceptance criteria

---
+++ src/test/java/de/galvanize/autos/AutosControllerTests.java
@@ -0,0 +1,12 @@
+package de.galvanize.autos;
+
+public class AutosControllerTests {
+
+    // GET: /api/autos
+    // GET: /api/autos returns list of all autos in db
+    // GET: /api/autos returns 204 no autos found
+    // GET: /api/autos?color=RED returns red cars
+    // GET: /api/autos?make=Ford returns fords
+    // GET: /api/autos?make=Ford&color=GREEN
+
+}
