e3af58ff26b659a4f9f50053510e4d34cb596374
SerschFischer <sergej.knjasev@me.com>
2022-02-23 12:23:02 +0100

    intilizr

---
+++ src/test/java/de/galvanize/autos/AutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.galvanize.autos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
